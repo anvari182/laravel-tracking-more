02529da087cb5d1461e9efbe09aecc419a07487b
anvari182 <anvari182@gmail.com>
2022-12-26 21:53:45 +0100

    Initial commit

---
+++ src/LaravelTrackingMoreServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Anvari182\LaravelTrackingMore;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelTrackingMoreServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}

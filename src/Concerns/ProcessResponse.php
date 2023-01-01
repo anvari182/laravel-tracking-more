e390d5dbb19c1b1930e85d56c970dc9d6743dfd4
anvari182 <anvari182@gmail.com>
2023-01-01 20:59:00 +0100

    Move methods

--- src/Concerns/ProcessResponse.php
+++ src/Concerns/ProcessResponse.php
@@ -2,10 +2,6 @@
 
 namespace Anvari182\TrackingMore\Concerns;
 
-use Anvari182\TrackingMore\Exceptions\EmptyResponseException;
-use Exception;
-use Illuminate\Http\Client\Response;
-
 trait ProcessResponse
 {
     /**
@@ -23,40 +19,5 @@ trait ProcessResponse
         }
 
         return true;
-    }
-
-    /**
-     * @param Response $response
-     * @return void
-     * @throws EmptyResponseException
-     * @throws Exception
-     */
-    public function processMeta(Response $response): void
-    {
-        $meta = $response->collect('meta');
-
-        if ($meta->isEmpty()) {
-            throw new EmptyResponseException;
-        }
-
-        if (!$this->isSuccessful($meta->get('code'))) {
-            throw new Exception($meta['message'] ?? 'TrackingMore request failed.');
-        }
-    }
-
-    /**
-     * @param Response $response
-     * @param string $key
-     * @return array
-     */
-    public function getResponseData(Response $response, string $key = 'data'): array
-    {
-        $data = $response->collect($key);
-
-        if ($data->isEmpty()) {
-            return [];
-        }
-
-        return $data->toArray();
     }
 }
